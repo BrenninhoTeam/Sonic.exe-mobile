346c0f90563507018961e4813ec5b4faea392667
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-15 14:33:41 -0300

    Atualizar o PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,7 +7,7 @@ import openfl.ui.Keyboard;
 import openfl.events.KeyboardEvent;
 import Replay.Ana;
 import Replay.Analysis;
-#if cpp
+#if desktop
 import webm.WebmPlayer;
 #end
 import flixel.input.keyboard.FlxKey;
