2b925f008c4261bc33abaece50e32da2baf85437
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-15 14:34:11 -0300

    Atualizar o TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -18,7 +18,7 @@ import flixel.input.gamepad.FlxGamepad;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
 import flixel.tweens.FlxEase.EaseFunction;
-import flixel.sound.FlxSound;
+import flixel.system.FlxSound;
 import flixel.system.ui.FlxSoundTray;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
